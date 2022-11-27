1c5ec2057a28cbf6a02d2f39f1a8a5c4728d76f1
manhattan404 <76757456+manhattan404@users.noreply.github.com>
2022-11-27 03:28:50 -0500

    Update ui.py

--- ui.py
+++ ui.py
@@ -5,3 +5,8 @@ from tkinter import ttk
 import customtkinter
 from customtkinter import *
 from PIL import ImageTk, Image
+
+# Create a frame that takes in question and answer and populate the SQL database
+
+def test():
+    pass
