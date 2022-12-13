8da7015a54aca828c20e3ff78840660a66ca140a
manhattan404 <76757456+manhattan404@users.noreply.github.com>
2022-12-13 18:08:39 -0500

    Update login2.py

--- login2.py
+++ login2.py
@@ -1,6 +1,15 @@
 import tkinter as tk
 
+
+grey_one = '#222222'
+grey_two = '#333333'
+light_black = '#111111'
+black = '#000000'
+
 root = tk.Tk()
+root.title("Log in 2")
+root.geometry('1300x750+370+160')
+root.configure(bg=grey_one)
 
 
 def login():
