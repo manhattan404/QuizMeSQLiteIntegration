84209f41c946eb8e1acd6660b261ae851d55633b
manhattan404 <76757456+manhattan404@users.noreply.github.com>
2023-01-12 21:43:29 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
+import customtkinter
+from customtkinter import *
 
 
 class Login:
@@ -22,8 +24,8 @@ class Login:
                                 bg='white', font=('Microsoft Yahei UI Light', 11))
         self.user_entry.place(x=65, y=375)
         self.user_entry.insert(0, 'Username')
-        self.user_entry.bind("<FocusIn>", on_enter)
-        self.user_entry.bind("<FocusOut>", on_leave)
+        self.user_entry.bind("<FocusIn>", self.clear_entry)
+        self.user_entry.bind("<FocusOut>", self.leave_entry)
         self.user_entry_frame = Frame(
             self.loginwin, width=207, height=1, bg='#111111').place(x=65, y=400)
 
@@ -32,19 +34,37 @@ class Login:
                                     bg='white', font=('Microsoft Yahei UI Light', 11))
         self.password_entry.place(x=65, y=435)
         self.password_entry.insert(0, 'Password')
-        self.password_entry.bind("<FocusIn>")  # on_enter)
-        self.password_entry.bind("<FocusOut>")  # on_leave)
+        self.password_entry.bind("<FocusIn>", self.clear_pass)  # on_enter)
+        self.password_entry.bind("<FocusOut>", self.clear_pass)  # on_leave)
         self.password_entry_frame = Frame(
             self.loginwin, width=207, height=1, bg='#111111').place(x=65, y=460)
 
+        # LOG IN BUTTON
+        self.login_bttn = customtkinter.CTkButton(
+            master=self.loginwin, width=120, height=32, text="Login", border_width=0, corner_radius=15, fg_color="#5F9DF7", border_color="white", text_color="white", bg_color="white", font=("Ebrima", 15))
+        self.login_bttn.place(x=107, y=490)
 
-def on_enter(self):
-    user_entry.delete(0, 'end')
+        # SIGN UP BUTTONS
+        self.signup_label = Label(self.loginwin, text="Don't have an account?", fg='black',
+                                  bg='white', font=('Microsoft YaHei UI Light', 9))
+        self.signup_label.place(x=65, y=535)
+        self.signup_bttn = Button(self.loginwin, width=6, text='Sign up', border=0,
+                                  bg='white', cursor='hand2', fg='#57a1f8')
+        self.signup_bttn.place(x=200, y=535)
 
+    def clear_entry(self, event):
+        self.user_entry.delete(0, END)
 
-def on_leave(self):
-    if self.user_entry.get() == '':
-        self.user_entry.insert(0, 'Username')
+    def clear_pass(self, event):
+        self.password_entry.delete(0, END)
+
+    def leave_entry(self, event):
+        if self.user_entry.get() == '':
+            self.user_entry.insert(0, 'Username')
+
+    def leave_password(self, event):
+        if self.password_entry.get() == '':
+            self.password_entry.insert(0, 'Password')
 
 
 def loginWin():
